cfd1d8cd6733dd4d715e71be958874a3d4ed535b
Ray Vernagus <r.vernagus@gmail.com>
2009-08-02 13:22:40 -0400

    Add Compiler with output specified context.

--- test/compiler_test.rb
+++ test/compiler_test.rb
@@ -9,9 +9,23 @@ class CompilerTest < Test::Unit::TestCase
       @compiler = IronRubyInline::Compiler.new
     end
     
-    should "have temp file name for parameters output" do
+    should "have temp file name for output parameter" do
       result = @compiler.parameters.output
       assert_equal "temp_file_name", result
+    end
+  end
+  
+  context "with output specified" do
+    setup do
+      flexmock(IronRubyInline::Path).
+        should_receive(:tmpfile).
+        never
+      @compiler = IronRubyInline::Compiler.new("output")
+    end
+    
+    should "have expected output parameter" do
+      result = @compiler.parameters.output
+      assert_equal "output", result
     end
   end
 end
