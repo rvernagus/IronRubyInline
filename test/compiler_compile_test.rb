ced196dd4f8f3670667e3df6c434c4da14825960
Ray Vernagus <r.vernagus@gmail.com>
2009-08-02 22:24:16 -0400

    Add when output is specified test.

--- test/compiler_compile_test.rb
+++ test/compiler_compile_test.rb
@@ -2,20 +2,27 @@ require "test_helper"
 
 class CompilerCompileTest < Test::Unit::TestCase
   include IronRubyInline
-  
+
   context "when no output is specified" do
     setup do
+      def Compiler.compile_from_parameters(code, parameters)
+        $parameters = parameters
+      end
       flexmock(IronRubyInline::Path).
         should_receive(:tmpfile => "tempfilename").
         once
     end
     
     should "use temp file name for output parameter" do
-      def Compiler.compile_from_parameters(code, parameters)
-        $parameters = parameters
-      end
       IronRubyInline::Compiler.compile("code")
       assert_equal "tempfilename", $parameters.output
+    end
+  end
+  
+  context "when output is specified" do
+    should "use specified file name for output parameter" do
+      IronRubyInline::Compiler.compile("code", :output => "output")
+      assert_equal "output", $parameters.output
     end
   end
 end
