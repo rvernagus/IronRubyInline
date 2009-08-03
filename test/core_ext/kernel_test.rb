35900bced5afdfeffeabe60386e43aae8bdd4a2f
Ray Vernagus <r.vernagus@gmail.com>
2009-08-03 09:30:36 -0400

    Add core_ext specs.

---
+++ test/core_ext/kernel_test.rb
@@ -0,0 +1,27 @@
+require File.dirname(__FILE__) + "/../test_helper"
+
+class KernelTest < Test::Unit::TestCase
+  context "use" do
+    setup do
+      @mock_disposable = flexmock("Disposable")
+      @mock_disposable.
+        should_receive(:dispose).
+        once
+    end
+    
+    should "yield the passed object" do
+      was_called = false
+      use(@mock_disposable) do |obj|
+        assert_same obj, @mock_disposable
+        was_called = true
+      end
+      assert was_called
+    end
+    
+    should "evaluate to result of yield" do
+      expected = Object.new
+      result = use(@mock_disposable) { expected }
+      assert_same result, expected
+    end
+  end
+end
