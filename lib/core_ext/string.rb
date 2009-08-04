9c974cc4344c9dd94abde0c995199e4cd3871f07
Ray Vernagus <r.vernagus@gmail.com>
2009-08-03 21:37:53 -0400

    Allow references to be passed into compile.

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -1,7 +1,10 @@
 class String
   def compile(options={})
     language = options[:language] || :cs
+    parameters = IronRubyInline::CompilerParameters.new
+    parameters.references = options[:references] || []
+    
     compiler = IronRubyInline::Compiler.new(language)
-    compiler.compile_and_load(self)
+    compiler.compile_and_load(self, parameters)
   end
 end
